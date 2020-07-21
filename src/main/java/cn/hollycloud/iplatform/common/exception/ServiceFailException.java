c22d20ca984c69476a8815e202a6c6e5ac7f1e47
huating <914483467@qq.com>
2020-07-21 09:02:17 +0800

    1.4 异常统一用serviceFail

--- src/main/java/cn/hollycloud/iplatform/common/exception/ServiceFailException.java
+++ src/main/java/cn/hollycloud/iplatform/common/exception/ServiceFailException.java
@@ -14,22 +14,59 @@ import lombok.Data;
 @Data
 public class ServiceFailException extends RuntimeException {
     private int status = Result.FAIL;
+    private boolean print = false;
 
     public ServiceFailException(String message) {
-        super(message);
+        this(message, false);
     }
 
     public ServiceFailException(String message, int status) {
-        super(message);
-        this.status = status;
+        this(message, status, false);
+    }
+
+
+    public ServiceFailException(String message, boolean print) {
+        this(message, Result.FAIL, print);
     }
 
     public ServiceFailException(BaseErrorCode errorCode) {
-        this(errorCode.getDesc(), errorCode.getValue());
+        this(errorCode, false);
+    }
+
+    public ServiceFailException(BaseErrorCode errorCode, boolean print) {
+        this(errorCode.getDesc(), errorCode.getValue(), print);
+    }
+
+    public ServiceFailException(String message, int status, boolean print) {
+        super(message);
+        this.status = status;
+        this.print = print;
+    }
+
+    public ServiceFailException(String message, int status, boolean print, Throwable cause) {
+        super(message, cause);
+        this.status = status;
+        this.print = print;
+    }
+
+    public ServiceFailException(String message, int status, Throwable cause) {
+        this(message, status, false, cause);
     }
 
     public ServiceFailException(String message, Throwable cause) {
-        super(message, cause);
+        this(message, Result.FAIL, cause);
+    }
+
+    public ServiceFailException(String message, boolean print, Throwable cause) {
+        this(message, Result.FAIL, print, cause);
+    }
+
+    public ServiceFailException(BaseErrorCode errorCode, boolean print, Throwable cause) {
+        this(errorCode.getDesc(), errorCode.getValue(), print, cause);
+    }
+
+    public ServiceFailException(BaseErrorCode errorCode, Throwable cause) {
+        this(errorCode.getDesc(), errorCode.getValue(), false, cause);
     }
 }
 
